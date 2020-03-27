4c9b784743c359e8d72e6dc894a501d7234a656b
Tiana Johnson <tiana.marie013@gmail.com>
2020-03-26 20:46:28 -0600

    i guess i am going to do a gulp tutorial cuz I am stuck

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,46 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+function defaultTask(cd) {
+    gulp.task('serve', ['sass'], function() {
 
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
-
-// Move the javascript files into our /src/js folder
-gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
-        .pipe(browserSync.stream());
-});
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-        server: "./src"  
+        browserSync.init({
+            server: "./src"  
+        });
+    
+        gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+        gulp.watch("src/*.html").on('change', browserSync.reload);
     });
+    cb();
+}
+exports.default = defaultTask
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+// var gulp        = require('gulp');
+// var browserSync = require('browser-sync').create();
+// var sass        = require('gulp-sass');
 
-gulp.task('default', ['js','serve']);
+
+// // Compile sass into CSS & auto-inject into browsers
+// gulp.task('sass', function() {
+//     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+//         .pipe(sass())
+//         .pipe(gulp.dest("src/css"))
+//         .pipe(browserSync.stream());
+// });
+
+// // Move the javascript files into our /src/js folder
+// gulp.task('js', function() {
+//     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+//         .pipe(gulp.dest("src/js"))
+//         .pipe(browserSync.stream());
+// });
+
+// // Static Server + watching scss/html files
+// gulp.task('serve', ['sass'], function() {
+
+//     browserSync.init({
+//         server: "./src"  
+//     });
+
+//     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+//     gulp.watch("src/*.html").on('change', browserSync.reload);
+// });
+
+// gulp.task('default', ['js','serve']);
