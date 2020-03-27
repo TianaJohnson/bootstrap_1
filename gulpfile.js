3a1026d25715a8fc50e41cd0fa7052ab686dea2f
Tiana Johnson <tiana.marie013@gmail.com>
2020-03-26 20:14:51 -0600

    node module paths linked to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ var gulp = require("gulp");
 var browserSync = require("browser-sync");
 var sass = require("gulp-sass");
 
+//Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
+});
+
 // Static Server + watch scss/html files
 gulp.task('serve', ['sass'], function(){
 
